42bbeef687ec41ea02aa9b92b32071d019ee93f4
Francis Ricci <fjricci@fb.com>
2016-04-15 12:28:53 -0700

    Add script to symlink config files to home directory

---
+++ createSymlinks.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+import sys
+
+homedir = os.environ["HOME"]
+configdir = os.path.realpath(os.path.dirname(sys.argv[0]))
+
+links = {}
+
+def _conf_path(path):
+    return os.path.join(configdir, path)
+
+def _home_path(path):
+    return os.path.join(homedir, path)
+
+def create_links():
+    for f in links:
+        dst = _home_path(f)
+        src = _conf_path(links[f])
+
+        if os.access(dst, os.F_OK):
+            print("error, could not link %s" % dst)
+            continue
+
+        print("linking  %s -> %s" % (dst, src))
+        if not os.path.exists(os.path.dirname(dst)):
+            os.makedirs(os.path.dirname(dst))
+        os.symlink(src, dst)
+
+if __name__ == "__main__":
+    create_links()
